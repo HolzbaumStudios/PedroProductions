3f5d3cd8cf334253ae818b455c6647a7240b8f56
fbHolzbaum <hhfblanc@CHHHMPC1000.eu.hcnet.biz>
2015-08-28 17:56:53 +0200

    Worked on the Terrain Generator

--- 2dProceduralGeneration/Minecraft2d/Assets/Scripts/TerrainGenerator.cs
+++ 2dProceduralGeneration/Minecraft2d/Assets/Scripts/TerrainGenerator.cs
@@ -20,8 +20,48 @@ public class TerrainGenerator : MonoBehaviour {
 		}
 	}
 
-	public int columns = 20;
-	public int rows = 10;
+
+
+	//This class saves the information of every terrain junk
+	[Serializable]
+	public class TerrainJunks
+	{
+		public int columns;
+		public int rows;
+		public Vector3[,] tilePosition;
+		public GameObject[,] tileObject;
+		public enum TerrainTypes {Standard, Mountain, Water};
+		public TerrainTypes terrainType;
+
+		//Function to initialize the class object
+		public TerrainJunks(int columnNr, int rowNr)
+		{
+			tilePosition = new Vector3[columnNr,rowNr];
+			tileObject = new GameObject[columnNr,rowNr];
+			int terrainTypeId = RandomTerrain();
+			switch(terrainTypeId)
+			{
+				case 0:terrainType = TerrainTypes.Standard;break;
+				case 1:terrainType = TerrainTypes.Mountain;break;
+				case 2:terrainType = TerrainTypes.Water;break;
+				default:terrainType = TerrainTypes.Standard;break;
+			}
+			Debug.Log (terrainType);
+		}
+
+		//Set a random terrain type and save it to the variable
+		public int RandomTerrain ()
+		{
+			//int enumSize = terrainType.GetNames(typeof(TerrainTypes)).Length;
+			int enumSize = 3;
+			int randomNumber = Random.Range(0, enumSize);
+
+			return randomNumber;
+		}
+	}
+
+	public int columns = 20; //Columns for each terrain Junk
+	public int rows = 20; //Rows for each terrain Junk
 	private int startPositionX;
 	private int startPositionY;
 	public GameObject[] dirtTiles;
@@ -30,21 +70,32 @@ public class TerrainGenerator : MonoBehaviour {
 	public GameObject terrainContainer;
 	private Transform tileHolder;
 
+	private List<TerrainJunks> terrainList = new List<TerrainJunks>();
 
-	private List <Vector3> gridPositions = new List<Vector3>();
+	//SetUp the next terrainJunk
+	void SetUpTerrainJunk()
+	{
+		TerrainJunks terrainObject = new TerrainJunks(columns, rows);
+		InitializePositionArray(ref terrainObject);
+		//TerrainSetup();
+	}
+
 
 	//Initialize a list of possible positions
-	void InitializeList()
+	void InitializePositionArray(ref TerrainJunks recievedTerrainObject)
 	{
-		gridPositions.Clear ();
+
+		startPositionX = 0;
+		startPositionY = 0;
 
 		for ( int x = -startPositionX; x < columns; x++)
 		{
 			for ( int y = -startPositionY; y < rows; y++)
 			{
-				gridPositions.Add (new Vector3(x,y,0f));
+				recievedTerrainObject.tilePosition[x,y] = new Vector3(x,y,0);
 			}
 		}
+
 	}
 
 	void TerrainSetup()
@@ -76,8 +127,7 @@ public class TerrainGenerator : MonoBehaviour {
 		startPositionX = columns / 2;
 		startPositionY = rows / 2;
 
-		InitializeList();
-		TerrainSetup ();
+		SetUpTerrainJunk();
 	}
 
 }
