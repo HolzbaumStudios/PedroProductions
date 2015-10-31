dfb7e629279d3cad06c7da493bfd6ae4c664ace2
fbHolzbaum <blancofel22@gmail.com>
2015-10-31 20:34:36 +0100

    Continued Board Setup

--- Breakout/Breakout/Assets/Scripts/SetUpBoard.cs
+++ Breakout/Breakout/Assets/Scripts/SetUpBoard.cs
@@ -3,26 +3,55 @@ using System.Collections;
 
 public class SetUpBoard : MonoBehaviour {
 
-	private float screenWidth = Screen.width;
-	private float screenHeight = Screen.height;
+	private Vector2 playerSize = new Vector2(20,2);
+	private Vector2 brickSize = new Vector2(12, 2);
 
-	private float playerWidth;
-	private float playerHeight;
 
 	public GameObject playerPrefab;
+	public GameObject brickPrefab;
 	private GameObject player;
 
-	// Use this for initialization
-	void Start () {
-		playerWidth = screenWidth / 10;
-		playerHeight = screenHeight / 10;
+	private int brickColumns = 8; //Always use an even number
+	private int brickRows = 2;
+	private int brickAmount = 24; //Alway use an even number
 
-		player = Instantiate (playerPrefab, new Vector2 (Screen.width / 2, Screen.height / 15), Quaternion.identity) as GameObject;
-		player.transform.localScale = new Vector3 (playerWidth, playerHeight, 0);
+	// Use this for initialization
+	void Start ()
+	{
+		SetUpPlayer ();
+		SetBoard ();
 	}
 	
-	// Update is called once per frame
-	void Update () {
-	
+	void SetUpPlayer()
+	{
+		player = Instantiate (playerPrefab, new Vector2 (0,-4.2f), Quaternion.identity) as GameObject;
+		player.transform.localScale = playerSize;
 	}
+
+	void SetBoard()
+	{
+		float brickXSpacing = 2;
+		float brickYSpacing = 0.5f;
+		float newXPosition = 1;
+		float newYPosition = 4;
+		int numberOfColumns = 0;
+		for (int i = 0; i < brickAmount/2; i++)
+		{
+			numberOfColumns += 2; 
+			GameObject brick = Instantiate (brickPrefab, new Vector2 (newXPosition,newYPosition), Quaternion.identity) as GameObject;
+			brick.transform.localScale = brickSize;
+			brick = Instantiate (brickPrefab, new Vector2 (-newXPosition,newYPosition), Quaternion.identity) as GameObject;
+			brick.transform.localScale = brickSize;
+			//Set new xposition
+			newXPosition += brickXSpacing;
+			//If reached maximum of columns, change row
+			if(numberOfColumns >= brickColumns)
+			{
+				newYPosition -= brickYSpacing;
+				numberOfColumns = 0;
+				newXPosition = 1;
+			}
+		}
+	}
+
 }
