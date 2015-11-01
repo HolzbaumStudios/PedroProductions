5d04d835fb25c4f9fedf9bff1676dee27b160f7a
fbHolzbaum <blancofel22@gmail.com>
2015-11-01 20:47:46 +0100

    Added Paddle and ball movement

---
+++ Breakout/Breakout/Assets/Scripts/ballScript.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ballScript : MonoBehaviour {
+
+	public float ballInitialVelocity = 600f;
+	
+	
+	private Rigidbody2D rigBody;
+	private bool ballInPlay = false;
+
+
+	void Awake ()
+	{
+		rigBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetButtonDown("Fire1") && ballInPlay == false)
+		{
+			Debug.Log("Fired");
+			transform.parent = null;
+			ballInPlay = true;
+			rigBody.isKinematic = false;
+			rigBody.AddForce(new Vector2(ballInitialVelocity, ballInitialVelocity));
+		}
+	}
+}
---
+++ Breakout/Breakout/Assets/Scripts/barMovement.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class barMovement : MonoBehaviour {
+
+	public float paddleSpeed;
+	private Vector3 playerPos = new Vector3 (0, -4.2f, 0);
+
+	// Update is called once per frame
+	void Update () {
+		float xPos = transform.position.x + (Input.GetAxis("Horizontal") * paddleSpeed);
+		playerPos = new Vector3 (Mathf.Clamp (xPos, -6.6f, 6.6f), -4.2f, 0f);
+		transform.position = playerPos;
+	}
+}
