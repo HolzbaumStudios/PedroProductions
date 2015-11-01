77353fa6a5812c4cb75d419755a3289f073897cf
fbHolzbaum <blancofel22@gmail.com>
2015-11-01 21:31:58 +0100

    Added brick collisions

---
+++ Breakout/Breakout/Assets/Scripts/brickScript.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class brickScript : MonoBehaviour {
+
+	void OnCollisionEnter2D (Collision2D collidingObject)
+	{
+		Debug.Log ("Collision");
+		Destroy (this.gameObject);
+	}
+}
